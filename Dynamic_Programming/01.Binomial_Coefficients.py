9f249fe26fabf4c54a0d7df350a253cd02e72bbc
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-08-22 16:45:11 +0300

    solve problem

---
+++ Dynamic_Programming/01.Binomial_Coefficients.py
@@ -0,0 +1,15 @@
+def calc_binom(n, k, memo):
+    key = f'{n} {k}'
+    if key in memo:
+        return memo[key]
+    if n == 0 or k == 0 or n == k:
+        return 1
+
+    res = calc_binom(n -1, k - 1, memo) + calc_binom(n - 1, k, memo)
+    memo[key] = res
+    return res
+
+n = int(input())
+k = int(input())
+
+print(calc_binom(n, k, {}))
